8d55c8feb46b4a3c78ee0420be17ab3a011f9d72
Messier81 <nesanyorku@gmail.com>
2019-07-25 02:15:39 -0400

    adds top bar layout and changes background color

---
+++ src/components/top_bar/TopBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './TopBar.css'
+
+function TopBar() {
+    return (
+        <nav className="TopBar">
+            <a href="/">Home</a>
+            <a href="/">Search</a>
+            <a href="/">Contact</a>
+            <a href="/">Login</a>
+
+            <a href="/">Login</a>
+
+            <a href="/">Login</a>
+        </nav>
+    )
+}
+
+export default TopBar
