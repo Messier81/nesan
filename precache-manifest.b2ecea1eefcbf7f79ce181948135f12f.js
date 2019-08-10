6876860017f9f2e67db3759544a2f4d26cbb177e
Messier81 <nesanyorku@gmail.com>
2019-08-09 22:28:58 -0400

    Updates

--- precache-manifest.b2ecea1eefcbf7f79ce181948135f12f.js
+++ precache-manifest.b2ecea1eefcbf7f79ce181948135f12f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d2884254c32e6e770a66ab130e85f542",
+    "revision": "03e031ddccee9a4039a091ced3c29f71",
     "url": "/pweb/index.html"
   },
   {
-    "revision": "ebd9fe03ed2345236c1c",
+    "revision": "d89dad6afe7a3cef82dd",
     "url": "/pweb/static/css/main.30eb3082.chunk.css"
   },
   {
-    "revision": "2a030385037c72458674",
-    "url": "/pweb/static/js/2.07af2cd4.chunk.js"
+    "revision": "c95de4209fac7656d5ff",
+    "url": "/pweb/static/js/2.5e77650d.chunk.js"
   },
   {
-    "revision": "ebd9fe03ed2345236c1c",
-    "url": "/pweb/static/js/main.acf52a22.chunk.js"
+    "revision": "d89dad6afe7a3cef82dd",
+    "url": "/pweb/static/js/main.55b1c566.chunk.js"
   },
   {
     "revision": "d3482088402870690e22",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pweb/precache-manifest.2a161a606336d5e6bc98209e4926fc77.js"
+  "/pweb/precache-manifest.b2ecea1eefcbf7f79ce181948135f12f.js"
 );
 
 self.addEventListener('message', (event) => {
