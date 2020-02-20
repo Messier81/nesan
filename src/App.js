883ba0e788e0b8979a6cee6750c0924b884f2244
Messier81 <nesanyorku@gmail.com>
2020-02-20 00:39:38 -0500

    added router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css'
 import Front from './comps/Front'
 import Nav from './comps/Nav'
 import Contact from './comps/Contact'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Front />
-      <Contact />
-    </div >
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/about" component={Front} />
+        <Route path="/contact" component={Contact} />
+      </div >
+    </Router>
   )
 }
 
