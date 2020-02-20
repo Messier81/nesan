9be4d14704becea969759275cb9c97786458e023
Messier81 <nesanyorku@gmail.com>
2020-02-20 00:21:54 -0500

    added contact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
 import Front from './comps/Front'
+import Nav from './comps/Nav'
+import Contact from './comps/Contact'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Front />
+      <Contact />
     </div >
   )
 }
---
+++ src/comps/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css'
+
+function Nav() {
+    return (
+        <nav>
+            <ul className="nav-links">
+                <li>Front</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
