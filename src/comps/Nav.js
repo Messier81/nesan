dd37e6ecb7988ecff110dc5a098cf440d798c88f
Messier81 <nesanyorku@gmail.com>
2020-02-20 01:54:05 -0500

    added router link nav

--- src/comps/Nav.js
+++ src/comps/Nav.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import '../App.css'
+
+import './Nav.css'
+
 import { Link } from 'react-router-dom'
+
+// Navigation bar
 
 function Nav() {
     return (
         <nav>
             <ul className="nav-links">
-                <Link to="/about">
-                    <li>Front</li>
-                </Link>
                 <Link to="/contact">
                     <li>Contact</li>
                 </Link>
+                <li><a href="https://github.com/Messier81">GitHub</a></li>
             </ul>
         </nav>
     )
