f9707e30fa6fc345dbe6b1f46af8496418597570
Messier81 <nesanyorku@gmail.com>
2020-04-19 23:48:31 -0400

    added lct

--- src/comps/Projects.js
+++ src/comps/Projects.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 function Projects() {
   return (
     <div className="Projects">
-      <Link to="/gtest">
-        <li>Graph Tester</li>
+      <Link to="/lct">
+        <li>Linux Commands Tester</li>
       </Link>
     </div>
   );
