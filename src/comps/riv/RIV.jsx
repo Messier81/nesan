1c07bf8971248c8b618d8a376274ffccb90ba364
Messier81 <nesanyorku@gmail.com>
2020-05-24 22:41:28 -0400

    added search for RIV

--- src/comps/riv/RIV.jsx
+++ src/comps/riv/RIV.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./RIV.css";
+import { Link } from "react-router-dom";
 
 export default class RIV extends Component {
   constructor(props) {
@@ -48,6 +49,7 @@ export default class RIV extends Component {
     });
   };
 
+  //Change index of array containing pictures so the next or the previous image is accessed, via adding/subtracting 1
   handlePicChange = (addV) => {
     this.setState({
       nextPic: this.state.nextPic + addV,
@@ -57,12 +59,25 @@ export default class RIV extends Component {
   render() {
     return (
       <div className="wrapper">
-        <button onClick={() => this.renderMyData(10)}>SEARCH</button>
-        <input type="text" name="subR" onChange={this.handleInputChange} />
-
-        <button onClick={() => this.handlePicChange(-1)}>PREVIOUS</button>
-
-        <button onClick={() => this.handlePicChange(1)}>NEXT</button>
+        <div className="topNav">
+          <div>
+            <Link to="/">
+              <button>Home</button>
+            </Link>
+          </div>
+          <div>
+            <div>
+              <button onClick={() => this.renderMyData(10)}>SEARCH</button>
+            </div>
+            <div>
+              <input
+                type="text"
+                name="subR"
+                onChange={this.handleInputChange}
+              />
+            </div>
+          </div>
+        </div>
 
         {this.state.data ? (
           <div class="container">
@@ -75,6 +90,10 @@ export default class RIV extends Component {
         ) : (
           <div>LOAD</div>
         )}
+
+        <button onClick={() => this.handlePicChange(-1)}>PREVIOUS</button>
+
+        <button onClick={() => this.handlePicChange(1)}>NEXT</button>
       </div>
     );
   }
