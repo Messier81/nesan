71cd4d5ac249a3e25ac93c23d0a0918f61eb795f
Messier81 <nesanyorku@gmail.com>
2020-05-28 01:43:19 -0400

    added front background

--- src/comps/Front.js
+++ src/comps/Front.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Nav from "./Nav";
+import "./Front.css";
 
 export default class Front extends Component {
   render() {
     return (
       <div>
         <Nav />
-        <h1>Hi, I'm Nesan.</h1>
+        <div className="mainTitle">
+          dasdasdsssssssssssssss dasd da das das hi
+        </div>
       </div>
     );
   }
