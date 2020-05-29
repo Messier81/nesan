e36bc314dee25e4dd1b53b25566c63c59eb70f68
Messier81 <nesanyorku@gmail.com>
2020-05-28 21:02:52 -0400

    Updates

---
+++ precache-manifest.a5d99cc2676af04aa1f89b4ae73e8d80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccc3b36492a076be18f1e9dac1d2a1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3002574f3796e5396767",
+    "url": "/static/css/main.dd1fb611.chunk.css"
+  },
+  {
+    "revision": "87cc9f37837f155e3c68",
+    "url": "/static/js/2.77611799.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/static/js/2.77611799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3002574f3796e5396767",
+    "url": "/static/js/main.4ed05fdc.chunk.js"
+  },
+  {
+    "revision": "74bf0402be6280f771b1",
+    "url": "/static/js/runtime-main.cf67fc8b.js"
+  },
+  {
+    "revision": "c5fccd04d749f34842263e23a3ba268c",
+    "url": "/static/media/toronto.c5fccd04.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pweb/precache-manifest.23bdef96c064a2c71fef084953c0e34c.js"
+  "/precache-manifest.a5d99cc2676af04aa1f89b4ae73e8d80.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pweb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
