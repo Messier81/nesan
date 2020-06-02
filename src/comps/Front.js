d2091c00c26f3ce5080062540bd6595d6f596652
Messier81 <nesanyorku@gmail.com>
2020-06-02 14:11:50 -0400

    router

--- src/comps/Front.js
+++ src/comps/Front.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
 import "./Front.css";
 
+//Bootstrap - for nav
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+import Jumbotron from "react-bootstrap/Jumbotron";
+
 export default class Front extends Component {
   render() {
     return (
       <div className="frontMain">
-        <div className="mainTitle">
-          <div className="navButtons">Projects</div>
-          <div className="mainDescription">Nesan V</div>
-          <div className="mainLinks">Github LinkedIn</div>
-        </div>
-        <div>hi</div>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Projects</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Jumbotron fluid className="frontMain">
+          <div className="myName">Hi, my name is Nesan.</div>
+
+          <div className="myFrom">I'm from</div>
+        </Jumbotron>
       </div>
     );
   }
